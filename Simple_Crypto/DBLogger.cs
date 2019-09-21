69283842e2116a31c821fab91f0c8ddf81428636
Dmitry Zhomir <giantdem@mail.ru>
2019-09-21 21:02:19 +0300

    Added DBLogger class

---
+++ Simple_Crypto/DBLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Simple_Crypto
+{
+    public static class DBLogger
+    {
+        public static void LogInput()
+        {
+
+        }
+    }
+}
