bb75b4e0be09cadaafc1f362f1f758efd2d625b0
Dmitry Zhomir <giantdem@mail.ru>
2019-09-21 20:59:33 +0300

    Added Encryptor class

---
+++ Simple_Crypto/Encryptor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Simple_Crypto
+{
+    public static class Encryptor
+    {
+        public static void Encrypt()
+        {
+
+        }
+    }
+}
