4798fb66970861d09fa8f02abb7450d5fbbeec77
Dmitry Zhomir <giantdem@mail.ru>
2019-09-23 02:40:43 +0300

    Main job coordinator

--- Simple_Crypto/Controllers/HomeController.cs
+++ Simple_Crypto/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Simple_Crypto.Controllers
 {
@@ -13,18 +9,14 @@ namespace Simple_Crypto.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public string EncryptMsg(string input)
         {
-            ViewBag.Message = "Your application description page.";
+            Entities e = new Entities();
 
-            return View();
-        }
+            string response = Encryptor.Encrypt(input, ref e);
+            DBLogger.LogInput(input, ref e);
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return response;
         }
     }
 }
