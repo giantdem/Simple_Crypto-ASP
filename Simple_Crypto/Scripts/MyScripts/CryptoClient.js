de429235c0b6c3575208dff43b262463f2e0c686
Dmitry Zhomir <giantdem@mail.ru>
2019-09-23 02:41:39 +0300

    Front-end and JS logic

---
+++ Simple_Crypto/Scripts/MyScripts/CryptoClient.js
@@ -0,0 +1,38 @@
+﻿var SimpleCrypto = angular.module('SimpleCrypto', []);
+
+SimpleCrypto.controller('CryptoController', function CryptoController($scope, CryptoService, $timeout) {
+    $scope.outputLog = [];
+
+    $scope.Encrypt = function (input) {
+        var errorAlert = function(msg) {
+            $scope.alert = true;
+            $scope.alertMsg = msg;
+            $timeout(function () { $scope.alert = false; }, 3000);
+        }
+
+        if (!input | input === "") {
+            errorAlert('пустой ввод');
+            return;
+        }
+
+        CryptoService.Encrypt(input)
+            .then(function (response) {
+                $scope.outputLog.push({ input: input, output: response.data });
+                console.log(response.data);
+            }, function (response) {
+                console.log(response.status);
+
+                errorAlert('не удалось получить ответ');
+            });
+    }
+});
+
+SimpleCrypto.factory('CryptoService', ['$http', function ($http) {
+    var CryptoService = {};
+
+    CryptoService.Encrypt = function (input) {
+        return $http.post('/Home/EncryptMsg', { input: input });
+    };
+
+    return CryptoService;
+}]);
